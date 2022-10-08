56cb5289ba3b40f16a5227eb44af18f02fc330a8
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-10-08 22:05:58 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,4 @@
+repeat task.wait() until game:IsLoaded()
+
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/eLeCtRaDoMiNuS/neverpeedv1/main/uilib.lua"))()
+local entity = loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/Libraries/entityHandler.lua", true))()
